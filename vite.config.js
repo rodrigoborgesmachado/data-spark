b116a02bf16e93218ef3a96c9be7b386b0a3392b
Rodrigo Machado <rodrigo.m@condocontrol.com>
2025-09-19 17:44:43 -0300

    Corrigindo

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
-// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/', // garanta isso
+  base: '/',
   plugins: [react()],
-});
+})
